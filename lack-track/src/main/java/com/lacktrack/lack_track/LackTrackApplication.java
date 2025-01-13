094f2a0171cc2d47f3c7024775303853b60bd2ba
ejagojo <agojo.eljohn@gmail.com>
2025-01-13 14:49:26 -0500

    first commit

---
+++ lack-track/src/main/java/com/lacktrack/lack_track/LackTrackApplication.java
@@ -0,0 +1,13 @@
+package com.lacktrack.lack_track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LackTrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LackTrackApplication.class, args);
+	}
+
+}
---
+++ lack-track/src/test/java/com/lacktrack/lack_track/LackTrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lacktrack.lack_track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LackTrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
